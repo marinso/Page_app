337ba9ccbca4692ab6a95d5797aaad6776e1a8e4
Martin <marinso404@gmail.com>
2019-02-15 11:32:06 +0100

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)   
-    if @user.save
-      log_in @user
+    user = User.new(user_params)   
+    if user.save
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to user
     else
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { email.downcase! }
     validates :name, presence: true, length: {minimum:3, maximum:50}
     validates :email, presence: true, length: {maximum: 255}, 
@@ -12,4 +13,23 @@ class User < ApplicationRecord
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
       end
+
+      def User.new_token
+        SecureRandom.urlsafe_base64
+      end
+
+      # Remembers a user in the database for use in persistent sessions.
+     def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
